0b31eb9530fdd1a4dc742f828b2d16e4c7247b2e
alex <ex@gmail.ru>
2021-12-22 00:14:56 +0500

    movie_2

--- app.js
+++ app.js
@@ -10,14 +10,33 @@ const personalMovieDB = {
     genres: [],
     pivat: false
 };
-const a = prompt('Один из последних просмотренных фильмов?',''),
-      b = prompt('На сколько оцените его?',''),
-      c = prompt('Один из последних просмотренных фильмов?',''),
-      d = prompt('На сколько оцените его?','');
 
-personalMovieDB.movies[a]=b;
-personalMovieDB.movies[c]=d;
 // personalMovieDB.movies.a=b;
 // personalMovieDB.movies.c=d;
-console.log(personalMovieDB)
+
+for (let i=0; i < 2; i++){
+    const a = prompt('Один из последних просмотренных фильмов?',''),
+          b = prompt('На сколько оцените его?','');
+          if(a != null && b != null && a != '' && a != '' && a.length < 50){
+              personalMovieDB.movies[a]=b;       
+              console.log('done');
+            } else{
+                console.log('eror');
+                i--;
+            }
+}
+if (personalMovieDB.count < 10 ){
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log( "Вы классический зритель");
+}else if(personalMovieDB.count >= 30){
+    console.log("Вы киноман");
+}else{
+    console.log("Произошла ошибка");
+}
+
+
+
+
+console.log(personalMovieDB);
 
